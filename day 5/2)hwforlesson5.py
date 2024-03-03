7dadc3eb252cb2ede80b0ba18d456d688f61953a
CoinAttic123350 <gorgadzesaba31@gmail.com>
2024-03-03 14:44:57 +0400

    second commit

---
+++ day 5/2)hwforlesson5.py
@@ -0,0 +1,12 @@
+print("Insert username")
+username = input()
+
+print("Insert age")
+age = input()
+
+print("Anwser this question: How many years later do you want to time travel?")
+anwser = input()
+
+print(username)
+print(age)
+print(anwser)
