c993ea143292152f458069b5b1dccd3f2c3b6951
CoinAttic123350 <gorgadzesaba31@gmail.com>
2024-03-09 19:51:06 +0400

    second commit

---
+++ day 5/hwday5.py
@@ -0,0 +1,18 @@
+
+#homework day 5
+
+
+
+number1 = (input("enter first number: "))
+number2 = (input("enter second number: "))
+
+
+print(type(number1))
+print(type(number2))
+
+num1 = float(number1)
+num2 = float(number2)
+
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
