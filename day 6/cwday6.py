7e445ce90ce47359ee9aa503b78f054838dc8c3d
CoinAttic123350 <gorgadzesaba31@gmail.com>
2024-03-17 14:53:08 +0400

    second commit

---
+++ day 6/cwday6.py
@@ -0,0 +1,11 @@
+
+#classwork day 6
+
+print(6 > 7)
+print(5 < 10)
+print(7 <= 8)
+print(10 == 10)
+
+
+num = input("enter number: ")
+print(num == 0)
---
+++ day 6/hwday6.py
@@ -0,0 +1,83 @@
+
+#homework day 6
+
+
+
+# 1)
+num1 = int(input("please enter a whole number: "))
+num2 = float(input("please enter a decimal number: "))
+
+print(num1 == num2)
+
+# 2)
+print(5 > 4)
+print(7 > 5)
+print(8 > 2)
+print(4 > 1)
+print(3 > 2)
+
+# 3)
+print(5 >= 3)
+print(8 >= 8)
+print(5 >= 4)
+print(4 >= 4)
+print(4 >= 2)
+
+# 4)
+print(3 < 5)
+print(5 < 9)
+print(3 < 8)
+print(2 < 9)
+print(3 < 7)
+
+# 5)
+print(5 <= 7)
+print(6 <= 6)
+print(3 <= 5)
+print(8 <= 8)
+print(2 <= 7)
+
+# 6)
+print(6 == 6)
+print(4 == 4)
+print(3 == 3)
+print(8 == 8)
+print(1 == 1)
+
+# 7)
+print(5 != 6)
+print(6 != 9)
+print(4 != 5)
+print(7 != 3)
+print(6 != 5)
+
+# 8)
+numb1 = int(input("enter first number: "))
+numb2 = int(input("enter second number: "))
+
+
+print(numb1 > numb2)
+print(numb1 >= numb2)
+print(numb1 < numb2)
+print(numb1 <= numb2)
+print(numb1 == numb2)
+print(numb1 != numb2)
+
+
+# 9)
+number1 = int(input("enter a number: "))
+number2 = number1 + 10
+
+print(number1 > number2)
+print(number1 >= number2)
+print(number1 < number2)
+print(number1 <= number2)
+print(number1 == number2)
+print(number1 != number2)
+
+# 10)
+a = int(input("enter a whole number: "))
+b = float(input("enter a decimal number: "))
+
+print(a == b)
+print(a != b)
