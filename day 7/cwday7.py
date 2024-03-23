e40159326fad89e86e192f20ff6bc7c14190aaf9
CoinAttic123350 <gorgadzesaba31@gmail.com>
2024-03-23 15:04:00 +0400

    second commit

---
+++ day 7/cwday7.py
@@ -0,0 +1,6 @@
+num = int(input("enter a number: "))
+print(num >= 0 and num < 5)
+
+num1 =  int(input("enter a number: "))
+print(num1 > 10)
+print(num1 // 2)
---
+++ day 7/hwday7.py
@@ -0,0 +1,16 @@
+# homework day 7
+
+celsius = int(input("enter a temperature in celsius: "))
+farenheit = ((celsius * 1.8) + 32)
+print(farenheit)
+
+age = int(input("enter your age: "))
+print((age < 20) and (age > 14))
+
+side1 = float(input("enter the size of a smaller side on a rectangle: "))
+side2 = float(input("enter the size of a bigger side on the same rectangle: "))
+area = side1 * side2
+print(area)
+
+num = int(input("enter a number: "))
+print((num > 100) and (num % 9))
