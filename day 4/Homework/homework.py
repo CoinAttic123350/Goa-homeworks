c297eb634a5146d804d062898c08576f48216f33
CoinAttic123350 <gorgadzesaba31@gmail.com>
2024-04-13 20:20:00 +0400

    second commit

--- day 4/Homework/homework.py
+++ day 4/Homework/homework.py
@@ -1,3 +1,12 @@
+
+# 1)
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+
+print(num1 + num2)
+
+
+# 2)
 name = input('Please enter your name: ')
 age = input('Please enter your age: ')
 anwser = input('How many years in the future would you go if you could time travel?: ')
